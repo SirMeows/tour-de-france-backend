80da5899f02687db9a3dbe037d6558f8698e6443
SirMeows <araelbrighteyes@gmail.com>
2022-05-30 16:04:32 +0200

    Changed erranious attribute name in Team

--- src/main/java/special/person/templbackend/service/TeamService.java
+++ src/main/java/special/person/templbackend/service/TeamService.java
@@ -2,8 +2,18 @@ package special.person.templbackend.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import special.person.templbackend.entity.Team;
+import special.person.templbackend.repository.TeamRepository;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @Service
 public class TeamService {
+
+    private TeamRepository tRepository;
+
+    public Set<Team> getTeams() {
+        return tRepository.findAllSet();
+    }
 }
